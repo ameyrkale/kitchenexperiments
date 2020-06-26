af450944240cf2bc7cfba33dfb6fc31700cb9381
ameyrkale <ameyka1@gmail.com>
2020-06-26 15:42:00 -0600

    Add files via upload

---
+++ src/components/Recipe.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Recipe = ({ title, calories, image, ingredients, healthLabels }) => {
+  if (title) {
+    return (
+      <div>
+        <div className="card mb-3 recipe-card">
+          <div className="row no-gutters">
+            <div className="col-md-4 nopadding">
+              <img src={image} className="card-img" alt="" />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">
+                  {healthLabels.map(label => (
+                    <li>{label}</li>
+                  ))}
+                </p>
+                <p className="card-text">
+                  <small className="text-muted">Calories {calories}</small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>Yo What you wanna cook today ? </p>
+      </div>
+    );
+  }
+};
+
+export default Recipe;
+
+/* <h1>{title}</h1>
+        <ol>
+          {ingredients.map(ingredient => (
+            <li>{ingredient.text}</li>
+          ))}
+        </ol>
+        <p>Calories count : {calories}</p>
+        <img src={image} alt="" /> */
